f5f39c231ecfe0c9f292fcb1056e3e35c4ad9893
Anastasiia Somchyk <e4nastya@gmail.com>
2018-05-06 17:39:07 -0500

    print ex resume

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,19 +1,24 @@
-const writeLocations = require('./dom');
+const dom = require('./dom');
 const loadLocation = require('./location');
+const loadEx = require('./ex');
 
-const whenLocationLoad = (data) => {
-  console.log('data', data);
-  $('#localPlace').append(writeLocations(data.locations));
-  // binEvents();
-  // applySale();
+const whenExLoads = function (data) {
+  $('#ex-resume').append(dom.printExToDom(data.dude));
 };
 
-const whenLocationDontLoad = (error) => {
+const whenLocationLoad =  function (data) {
+  console.log('data', data);
+  $('#localPlace').append(dom.writeLocations(data.locations));
+
+};
+
+const whenDontLoad = function (error) {
   console.log('this is error', error);
 };
 
 const initializer = () => {
-  loadLocation(whenLocationLoad, whenLocationDontLoad);
+  loadEx(whenExLoads, whenDontLoad);
+  loadLocation(whenLocationLoad, whenDontLoad);
 };
 
 module.exports = initializer;
--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,18 +1,42 @@
+const printExToDom = (dude) => {
+  let domStrong = '';
+  dude.forEach((ex) => {
+    domStrong += `<div class="jumbotron">
+    <img class="snuggle-bunny" src="${ex.imageSoure}">
+    <div class="pull-right">
+     <h1 class="display-4">${ex.name}</h1>
+     <h2>Age ${ex.age}</h2>
+     <p class="lead">${ex.flaws}</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to    space content out within the larger container.</p>
+      <p class="lead">
+      <button type="button" class="btn btn-outline-dark">Morning</button>
+      <button type="button" class="btn btn-outline-dark">Afternoon</button>
+      <button type="button" class="btn btn-outline-dark">Evening</button>
+      <button type="button" class="btn btn-outline-dark">After Dark</button>
+  </p>
+  </div>
+  </div>`;
+  });
+  return domStrong;
+};
+
 const writeLocations = (locations) => {
-  let domString = '';
+  let domStrang = '';
   locations.forEach((location) => {
-    domString += `<div class="card col-md-6 col-md-offset-3">
-     <div class="thumbnail">
-     <h1>${location.localName}</h1>
-      <img src="${location.imageSoure}">
+    domStrang += `<div class="card col-md-4">
+     <h2>${location.localName}</h2>
+      <img class="img-ex" src="${location.imageSoure}">
         <div class="caption">;
        <h3 id="thumbnail-label"> ${location.localAddress}</h3>
        </div>
-      </div>
     </div>
     `;
   });
-  return domString;
+  return domStrang;
 };
 
-module.exports = writeLocations;
+module.exports = {
+  printExToDom,
+  writeLocations,
+};
